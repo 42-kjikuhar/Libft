a606ff009fb52fe1b1dcc3df0cf08274202bd2fe
jiku <134150757+jiku0730@users.noreply.github.com>
2025-12-10 21:10:31 +0900

    fix: norminette header (#2)

--- ft_stdio/src/ft_printf_utils/ft_handle/ft_handle_char.c
+++ ft_stdio/src/ft_printf_utils/ft_handle/ft_handle_char.c
@@ -3,24 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_handle_char.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
+/*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:11:48 by kjikuhar          #+#    #+#             */
-/*   Updated: 2025/07/14 11:56:43 by kei2003730       ###   ########.fr       */
+/*   Updated: 2025/12/10 21:07:06 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_utils.h"
 
-void	ft_handle_char(t_conv_spec *conv_info, int c)
-{
-	unsigned char	*casted_uc;
+void	ft_handle_char(t_conv_spec *conv_info, int c) {
+  unsigned char *casted_uc;
 
-	casted_uc = malloc(sizeof(unsigned char) * 2);
-	if (!casted_uc)
-		return (conv_info->validation = -1, (void)0);
-	casted_uc[0] = (unsigned char)c;
-	casted_uc[1] = '\0';
-	conv_info->output_str = (char *)casted_uc;
-	conv_info->output_length = 1;
+  casted_uc = malloc(sizeof(unsigned char) * 2);
+  if (!casted_uc)
+    return (conv_info->validation = -1, (void)0);
+  casted_uc[0] = (unsigned char)c;
+  casted_uc[1] = '\0';
+  conv_info->output_str = (char *)casted_uc;
+  conv_info->output_length = 1;
 }
--- ft_strings/src/ft_index.c
+++ ft_strings/src/ft_index.c
@@ -3,29 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_index.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
+/*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/13 20:12:11 by kei2003730        #+#    #+#             */
-/*   Updated: 2025/07/13 20:48:16 by kei2003730       ###   ########.fr       */
+/*   Created: 2025/07/13 20:12:11 by kjikuhar          #+#    #+#             */
+/*   Updated: 2025/12/10 21:06:25 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_strings.h"
 
-char	*ft_index(const char *s, int c)
-{
-	char	target;
+char	*ft_index(const char *s, int c) {
+  char target;
 
-	if (s == NULL)
-		return (NULL);
-	target = (char)c;
-	while (*s)
-	{
-		if (*s == target)
-			return ((char *)s);
-		s++;
-	}
-	if (target == '\0')
-		return ((char *)s);
-	return (NULL);
+  if (s == NULL)
+    return (NULL);
+  target = (char)c;
+  while (*s) {
+    if (*s == target)
+      return ((char *)s);
+    s++;
+  }
+  if (target == '\0')
+    return ((char *)s);
+  return (NULL);
 }
