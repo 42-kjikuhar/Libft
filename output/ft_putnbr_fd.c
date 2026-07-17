cf623f907b73c8687316b91948b5de055204bb25
jiku0730 <github@jiku.jp>
2026-07-17 21:04:42 +0900

    refactor: INT_MINの直書きを無くし、再帰部分を別関数にした。

--- output/ft_putnbr_fd.c
+++ output/ft_putnbr_fd.c
@@ -6,25 +6,31 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 14:33:56 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/01/21 10:38:22 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/07/17 21:04:21 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	putnbr_unsigned(unsigned int nb, int fd)
+{
+	if (nb > 9)
+		putnbr_unsigned(nb / 10, fd);
+	write(fd, &(char){(char)(nb % 10 + '0')}, 1);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
-	{
-		write(fd, "-2147483648", 11);
-		return ;
-	}
+	unsigned int	nb;
+
 	if (n < 0)
 	{
 		write(fd, "-", 1);
-		n *= -1;
+		nb = -(unsigned int)n;
 	}
-	if (n > 9)
-		ft_putnbr_fd(n / 10, fd);
-	write(fd, &(char){n % 10 + '0'}, 1);
+	else
+	{
+		nb = (unsigned int)n;
+	}
+	putnbr_unsigned(nb, fd);
 }
