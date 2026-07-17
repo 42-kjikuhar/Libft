5baba156726ecb72fca5281aeb04f09b0ff92ea6
jiku0730 <github@jiku.jp>
2026-07-17 21:01:24 +0900

    refactor: INT_MINを"-2147483648"と直接書いていたのを辞めて、unsigned int にcastすることでINT_MINの環境依存に対応した。

--- conversion/ft_itoa.c
+++ conversion/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 11:55:06 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/07/17 20:30:52 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/07/17 21:01:20 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,65 +15,49 @@
 /* Convert the integer 'n'to a ASCII string */
 static size_t	number_len_(int number)
 {
-	size_t	number_len;
+	unsigned int	nb;
+	size_t			number_len;
 
 	number_len = 1;
-	if (number == INT_MIN)
-	{
-		return (11);
-	}
 	if (number < 0)
 	{
-		number *= -1;
+		nb = -(unsigned int)number;
 		number_len++;
 	}
-	while (number >= 10)
+	else
+		nb = (unsigned int)number;
+	while (nb >= 10)
 	{
-		number /= 10;
+		nb /= 10;
 		number_len++;
 	}
 	return (number_len);
 }
 
-static char	*ft_strcpy(char *dest, char const *src)
-{
-	int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
 char	*ft_itoa(int n)
 {
-	char	*dest_str;
-	int		i;
+	char			*dest_str;
+	unsigned int	nb;
+	size_t			i;
 
-	dest_str = malloc(sizeof(char) * (number_len_(n) + 1));
+	i = number_len_(n);
+	dest_str = malloc(sizeof(char) * (i + 1));
 	if (!dest_str)
 		return (NULL);
-	if (n == -2147483648)
-	{
-		return (ft_strcpy(dest_str, "-2147483648"));
-	}
-	i = number_len_(n) - 1;
+	dest_str[i] = '\0';
 	if (n < 0)
 	{
 		dest_str[0] = '-';
-		n *= -1;
+		nb = -(unsigned int)n;
 	}
-	dest_str[i + 1] = '\0';
-	while (n >= 10)
+	else
+		nb = (unsigned int)n;
+	while (nb >= 10)
 	{
-		dest_str[i] = (char)(n % 10 + '0');
-		n /= 10;
 		i--;
+		dest_str[i] = (char)(nb % 10 + '0');
+		nb /= 10;
 	}
-	dest_str[i] = (char)(n % 10 + '0');
+	dest_str[i - 1] = (char)(nb + '0');
 	return (dest_str);
 }
