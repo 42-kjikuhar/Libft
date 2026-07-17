cae9600e127319a2d98b5098a37f863f3ecad38f
jiku0730 <github@jiku.jp>
2026-07-17 21:23:29 +0900

    refarctor: ft_strdupのcpy部分をft_memcpyに置き換え。変数名もわかりやすくした。

--- string/ft_strdup.c
+++ string/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 05:14:00 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/07/17 20:31:17 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/07/17 21:22:41 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,12 @@
 
 char	*ft_strdup(char const *s)
 {
-	char	*tmp;
-	char	*p;
+	char	*dest;
+	size_t	len;
 
-	tmp = (char *)malloc(sizeof(s[0]) * (ft_strlen(s) + 1));
-	if (tmp == NULL)
+	len = ft_strlen(s);
+	dest = malloc(sizeof(char) * (len + 1));
+	if (dest == NULL)
 		return (NULL);
-	p = tmp;
-	while (*s)
-	{
-		*tmp = *s;
-		s++;
-		tmp++;
-	}
-	*tmp = '\0';
-	return (p);
+	return (ft_memcpy(dest, s, len));
 }
