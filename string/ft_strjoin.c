038546aa14fcccb33a7b746ea2699573843bed00
jiku0730 <github@jiku.jp>
2026-07-17 20:40:31 +0900

    refactor: strjoin内でのstrcpyをstrlcpyに変更。

--- string/ft_strjoin.c
+++ string/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 20:47:54 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/07/17 20:31:20 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/07/17 20:38:12 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,20 +30,6 @@ static char	*ft_strcat(char *dest, char const *src)
 	return (dest);
 }
 
-static char	*ft_strcpy(char *dest, char const *src)
-{
-	size_t	i;
-
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*dest;
@@ -55,7 +41,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	dest = malloc(sizeof(char) * (sum_len + 1));
 	if (!dest)
 		return (NULL);
-	ft_strcpy(dest, s1);
+	ft_strlcpy(dest, s1, ft_strlen(s1));
 	ft_strcat(dest, s2);
 	return (dest);
 }
